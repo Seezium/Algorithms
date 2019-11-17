3b5619bb186a41b39dfc26358173e60c4dddb59b
Seezium <42362081+Seezium@users.noreply.github.com>
2019-11-17 09:26:29 -0800

    Update SumOfIntervals.py
    Level 4 Algorithm

--- Sum of Intervals/SumOfIntervals.py
+++ Sum of Intervals/SumOfIntervals.py
@@ -2,7 +2,7 @@ def sum_of_intervals(intervals):
     arr = []
     res = []
     for n in intervals:
-        for i in range(n[0],n[1]):
+        for i in range(n[0], n[1]):
             arr = arr + [i]
             arr.sort()
     for n in arr:
